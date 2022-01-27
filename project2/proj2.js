a40ef44466a4982d8b6cff46c77377285bb1708a
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-01-27 18:28:25 -0500

    Test 1

---
+++ project2/proj2.js
@@ -0,0 +1,13 @@
+// eli brooks
+// 1/25/22
+// animation and conditional operators
+
+// setup
+function setup() {
+    frameRate(1);
+}
+
+// call the draw function so numbers will start in the log
+function draw() {
+    console.log(frameCount);
+}

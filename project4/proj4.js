b14f87f2f7a7bf18fc281a893c7e9918264342b4
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-02-10 13:43:45 -0500

    update index and 4

--- project4/proj4.js
+++ project4/proj4.js
@@ -1,3 +1,8 @@
+// eli brooks
+// objects exercises
+// due 2/10
+
+// declare the ball object
 let objBall = {
     x: 100,
     y: 100,
@@ -5,6 +10,7 @@ let objBall = {
     size: 25,
 };
 
+// general setup 
 function setup() {
     createCanvas(500, 400);
 
@@ -13,6 +19,7 @@ function setup() {
     fill(objBall.ballColor);
 }
 
+// a second circle for the second prompt
 let objCircle = {
     x: 300,
     y: 100,
@@ -20,9 +27,10 @@ let objCircle = {
     offsetX: 100,
 }
 
+// calling the circle
 function draw() {
     circle(objCircle.x, objCircle.y, objCircle.size);
-    if(mousePressed) {
+    if(mouseIsPressed) {
         objCircle.offsetX = -1;
     }
 }

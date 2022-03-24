d7d05ca64063828bd5bf86c1887eb29860356e0f
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-03-24 14:02:50 -0400

    Proj7 variables added

--- project7/proj7.js
+++ project7/proj7.js
@@ -3,8 +3,11 @@ let dvWordOutput = document.getElementById("wordOutput");
 
 function logText() {
     let txtWord = txtName.value;
-    let word = `${txtWord} ${txtWord} ${txtWord}`;
-    dvWordOutput.innerHTML = "Hello, " + word;
+    let word = `${txtWord}`;
+    dvWordOutput.innerHTML = "Hello, " + word + ".";
 
     txtName.value =""
 }
+
+let numberName = document.getElementById("numberName");
+let dvNumberOutput = document.getElementById("numOutput");

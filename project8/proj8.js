aafd6563cf1113fe3e6c15466c64d36b4edba4e1
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-07 11:40:25 -0400

    up8

--- project8/proj8.js
+++ project8/proj8.js
@@ -5,16 +5,9 @@
 // make the div usable in js
 let dvNumOutput = document.getElementById("numOutput");
 
-//function picker() {
- //   let num = Math.floor(Math.random() * 10);
-//}
-
+// function to generate and display the #
 function genRandom() {
-    let num = Math.floor(Math.random() * 10);
+    let num = Math.floor(Math.random() * 11);
     dvNumOutput.innerHTML = "Number: " + num;
 }
 
-const months = ["January", "February", "March", "April", "May", "June", "July"];
-
-const random = Math.floor(Math.random() * months.length);
-console.log(random, months[random]);

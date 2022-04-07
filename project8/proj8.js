a320d26057210acffe041a9c655bf33f0c87290e
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-07 11:36:02 -0400

    logcheck

--- project8/proj8.js
+++ project8/proj8.js
@@ -13,3 +13,8 @@ function genRandom() {
     let num = Math.floor(Math.random() * 10);
     dvNumOutput.innerHTML = "Number: " + num;
 }
+
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);

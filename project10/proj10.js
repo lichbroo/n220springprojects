af77972ebec093c74417103ef6489462d6325d5f
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-14 18:53:27 -0400

    add10

---
+++ project10/proj10.js
@@ -0,0 +1,43 @@
+// eli brooks
+// 4/12/22
+// event exercises
+
+// declare colors
+let red = 0;
+let blue = 0;
+let green = 0; 
+
+let dvMain = document.getElementById('maindiv');
+let displayDiv = document.getElementById('displayDiv');
+
+// create display box
+dvMain.style.height ='200px';
+dvMain.style.width ='200px';
+
+// change color w/ functions in the display box
+changeColor();
+function changeColor(){
+
+    let color = "RGB(" +red + "," + green + "," + blue + ")"; 
+
+    dvMain.style.backgroundColor = color;
+
+    displayDiv.innerHTML = color;
+
+
+}
+
+// functions to change color values
+function moreRed(){
+
+    red += Number(event.target.getAttribute("data-num"));
+    changeColor();
+}
+function moreBlue(){
+    blue += Number(event.target.getAttribute("data-num"));
+    changeColor();
+}
+function moreGreen(){
+    green += Number(event.target.getAttribute("data-num"));
+    changeColor();
+}

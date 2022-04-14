3dae7153e65c01f6e9b1bfcf20f9f5c72fc05948
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-14 19:58:02 -0400

    update proj11

--- project11/proj11.js
+++ project11/proj11.js
@@ -2,6 +2,22 @@
 // 4/14/22
 // array computation exercises
 
+// compute average of numbers split by ,
+function avgScore(){
+    var scoreVar = userNum.value;
+    let scoreArray = scoreVar.split(",");
+    
+    let sum = 0;
+    for(let i = 0; i < scoreArray.length; i++ )
+    {
+        sum += Number(scoreArray[i]);
+    }
+    average = sum / scoreArray.length;
+    
+    document.getElementById("result").innerHTML = "Result: " + average;
+    
+    }
+
 // filter and count bad words
 function badWords(){
     let total = 0;
@@ -21,31 +37,29 @@ function badWords(){
         total += 1;
         }
         }
-        document.getElementById("result").innerHTML = "Number of Bad Words: " + total;
+        document.getElementById("result2").innerHTML = "Number of Bad Words: " + total;
     }
 
 // check for three 1's in input
 function winGame(){
     let gameArray = document.getElementById("winning").value;
     gameCount = gameArray.split(",");
-    console.log(gameCount);
     let count = 0;
     
-    // 
+    // run until numbers are done
     for(let i=0; i < gameCount.length; i++){
         if(gameCount[i] == "1"){
             count +=1;
-            console.log(count);
         }
         else 
         {
-            document.getElementById("result").innerHTML = "Winner";
+            document.getElementById("result3").innerHTML = "Winner";
             break;
         }
             if(count == 3) 
         {
             console.log(count);
-            document.getElementById("result").innerHTML = "Loser";
+            document.getElementById("result3").innerHTML = "Loser";
             break;
         }
     }

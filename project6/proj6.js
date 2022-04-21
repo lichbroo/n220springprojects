2f109c8984f06c9739fa8fa1bbae0f1c6ed39182
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-21 12:05:01 -0400

    update 6

--- project6/proj6.js
+++ project6/proj6.js
@@ -2,19 +2,24 @@
 // 4/20/22
 // loops and arrays exercises
 
-// for loop to rnu it 25 times
+// for loop to run 5 times
+for(let i = 1; i <= 25; i++){
+    document.getElementById("loop").innerHTML+= i + "000";
+}
+
+// for loop to run it 25 times
 for(let i = 1; i <= 25; i++){
     // beep
     if(i % 3 == 0){
-        document.getElementById('beepBop').innerHTML+= "Beep" + "<br>";
+        document.getElementById("beepBop").innerHTML+= "Beep" + "<br>";
     }
     // bop
     else if(i % 5 == 0){
-        document.getElementById('beepBop').innerHTML += "Bop" + "<br>";
+        document.getElementById("beepBop").innerHTML += "Bop" + "<br>";
     }
     // beepbop
    if(i % 3 == 0  && i % 5 == 0){
-        document.getElementById('beepBop').innerHTML += "Beep Bop" + "<br>";
+        document.getElementById("beepBop").innerHTML += "Beep Bop" + "<br>";
     }
 
 }

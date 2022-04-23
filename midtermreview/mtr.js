e4fc5d389889f4e131a86e0153d5451914c357df
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-04-23 16:35:13 -0400

    add mtr

---
+++ midtermreview/mtr.js
@@ -0,0 +1,23 @@
+// eli brooks
+// 4/23/22
+// miderm review exercises
+
+// place for game to run
+let game = document.getElementById("game");
+    game.style.display = "flex";
+    game.style.padding = "6px 12px";    
+
+// show which is chosen
+let rock = document.getElementById("rock");
+    rock.style.display = "flex";
+
+let paper = document.getElementById("paper");
+    paper.style.display = "flex";
+
+let scissors = document.getElementById("scissors");
+    scissors.style.display = "flex";
+
+let guard = document.getElementById("guard");
+    guard.style.display = "flex";
+
+let score = document.getElementById("score");
